a0e37b24abf48bf16cf9d392ef436cd1b7a673ec
Parth Dalwadi <pbdalwadi@gmail.com>
2019-12-13 07:03:58 -0500

    - Description updated to get intent for displaying all detains in textview.

--- app/src/main/java/com/example/parth_c0766346_fp/Description.java
+++ app/src/main/java/com/example/parth_c0766346_fp/Description.java
@@ -2,13 +2,22 @@ package com.example.parth_c0766346_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Description extends AppCompatActivity {
-
+    TextView info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
+
+        info = findViewById(R.id.details);
+
+        Intent i = getIntent();
+        String summary = i.getStringExtra("allInfo");
+        info.setText(summary);
+
     }
 }
